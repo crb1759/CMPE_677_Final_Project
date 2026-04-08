82d0408622be616b9546c0c802a51579cece3b73
connorbastian <crb1759@rit.edu>
2026-04-08 12:46:37 -0400

    Initial Framework : Missing Functionality

---
+++ src/src_parameters.py
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------------
+# File: src_parameters.py
+# Function: Helper File for Machine Learning
+# Author: Connor Bastian
+# Date: April 6, 2026
+# ----------------------------------------------------------------
+
+DATASET_LIST = "Video_Games_Sales_(1980-2024)_Raw.csv"
+
+RF_NUM_ITERATIONS = 10
